d7bda92a2347bfece981585191b521977473051c
Luke Lewis <lukemarius@gmail.com>
2018-05-01 13:20:55 -0700

    First Commit

---
+++ app/src/main/java/com/example/topsongsbyyear/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.topsongsbyyear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView yearsView;
+    private ArrayAdapter<String> yearsAdapter;
+    ArrayList<String> years;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        yearsView = findViewById(R.id.yearsView);
+        for(int i = 2006; i < 2018 ; i++){
+            years.add(Integer.toString(i));
+        }
+        yearsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, years);
+        yearsView.setAdapter(yearsAdapter);
+        yearsView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public
+}
---
+++ app/src/main/java/com/example/topsongsbyyear/RetrieveSongs.java
@@ -0,0 +1,10 @@
+package com.example.topsongsbyyear;
+
+import android.os.AsyncTask;
+
+public class RetrieveSongs extends AsyncTask<Void, Void, String[]> {
+    @Override
+    protected String[] doInBackground(Void... voids) {
+        return null;
+    }
+}
